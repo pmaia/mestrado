b5beae1b8a6849640f853f4052cc9fad7f7d1442
Patrick Maia <patrickjourdanmaia@gmail.com>
2013-03-26 20:48:56 -0300

    not sure is something really changed

--- experiments/experiment_01/run.sh
+++ experiments/experiment_01/run.sh
@@ -3,6 +3,6 @@
 for scenario in `ls ./scenarios`; do
 	mkdir ./results/$scenario
 	for i in `seq 1 30`; do
-		java -jar beefs-energy-simulation-ef73ba2.jar ./scenarios/$scenario > ./results/$scenario/result_$i  2> ./results/$scenario/error_$i.log 
+		java -jar beefs-energy-simulation-ef73ba2.jar ./scenarios/$scenario > ./results/$scenario/result_$i  2> ./results/$scenario/error_$i.log
 	done
 done
