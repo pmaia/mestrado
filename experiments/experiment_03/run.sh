62242fdf65b31a4910bf69b008177b27984a56d9
Patrick Maia <pmaia@thoughtworks.com>
2013-08-23 08:53:17 -0300

    filter error log in order to save disk space

--- experiments/experiment_03/run.sh
+++ experiments/experiment_03/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 for scenario in `ls ./scenarios`; do
-	mkdir ./results/$scenario
-	for i in `seq 1 30`; do
-		java -jar $1 ./scenarios/$scenario > ./results/$scenario/result_$i  2> ./results/$scenario/error_$i.log
-	done
+  mkdir ./results/$scenario
+  for i in `seq 1 30`; do
+    error_file=./results/$scenario/error_$i.log
+    java -jar $1 ./scenarios/$scenario > ./results/$scenario/result_$i  2> $error_file
+    grep -v "already ahead of" ./result/$scenarios/error_$i.log > temp_error
+    mv temp_error $error_file
+  done
 done
