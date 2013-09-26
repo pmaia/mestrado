53543042b9103063c463d9dbc52ae45333122f42
Patrick Maia <patrickjourdanmaia@gmail.com>
2013-09-26 09:07:55 -0300

    corrige um erro :)

--- experiments/experiment_03/run.sh
+++ experiments/experiment_03/run.sh
@@ -5,7 +5,7 @@ for scenario in `ls ./scenarios`; do
   for i in `seq 1 30`; do
     error_file=./results/$scenario/error_$i.log
     java -jar $1 ./scenarios/$scenario > ./results/$scenario/result_$i  2> $error_file
-    grep -v "already ahead of" ./result/$scenarios/error_$i.log > temp_error
+    grep -v "already ahead of" ./results/$scenario/error_$i.log > temp_error
     mv temp_error $error_file
   done
 done
